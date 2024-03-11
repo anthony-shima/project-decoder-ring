affabf7a53bac07f173e4c03d926775691f08cfe
Anthony <shima.anthony@gmail.com>
2024-03-11 15:32:20 -0700

    Update polybius.js

--- src/polybius.js
+++ src/polybius.js
@@ -1,13 +1,75 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
+
+  const polybiusSquare = {
+    "a": "11", "b": "21", "c": "31", "d": "41", "e": "51",
+    "f": "12", "g": "22", "h": "32", "i": "42", "j": "42", "k": "52",
+    "l": "13", "m": "23", "n": "33", "o": "43", "p": "53",
+    "q": "14", "r": "24", "s": "34", "t": "44", "u": "54",
+    "v": "15", "w": "25", "x": "35", "y": "45", "z": "55"
+  };
+
+  const reversePolybiusSquare = {}
+
+  for (let letter in polybiusSquare) {
+    const numberPair = polybiusSquare[letter];
+
+    if (reversePolybiusSquare[numberPair]) {
+      reversePolybiusSquare[numberPair] = `(${reversePolybiusSquare[numberPair]}/${letter})`
+    } else {
+      reversePolybiusSquare[numberPair] = letter;
+    }
+
+  }
+
+  function stringArray(string) {
+    return Array.from(string);
+  }
+
+  function encodeInput(input) {
+    const array1 = stringArray(input.toLowerCase());
+    return array1.map(character => {
+      if (character === " ") {
+        return character;
+      } else {
+        return polybiusSquare[character];
+      }
+    }).join("");
+  }
+
+  function decodeInput(input) {
+    const array1 = stringArray(input);
+    const decodedArray = [];
+    for (let i = 0; i < array1.length; i++) {
+      const index = array1[i];
+      if (index === " ") {
+        decodedArray.push(index);
+      }
+      else {
+        let tens = index;
+        let ones = array1[i + 1];
+        const numberKey = tens + ones;
+        i = i + 1;
+        decodedArray.push(reversePolybiusSquare[numberKey]);
+      }
+    }
+    return decodedArray.join("");
+  }
+
+
 
   function polybius(input, encode = true) {
-    // your solution code here
+
+    if (encode === false) {
+      if (input.replace(" ", "").length % 2 > 0) {
+        return false;
+      } else {
+        return decodeInput(input);
+      }
+    }
+
+    if (encode) {
+      return encodeInput(input);
+    }
   }
 
   return {
